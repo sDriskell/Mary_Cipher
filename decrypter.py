b6fa91a9f7246305d62020f329761e95f0727dd4
sDriskell <Driskell.Shane.L@gmail.com>
2020-12-07 10:01:29 -0500

    Initial version of solve_cipher(message_list, count) has been created.  Needs to be given a loop feature to iterate over the list of words to create different results for the user to see.

--- decrypter.py
+++ decrypter.py
@@ -11,27 +11,31 @@ def load_text(file):
         return f.read().strip()
 
 
-def solve_cipher(message_list):  # code message: drinkovaltine
+def solve_cipher(message_list, count):  # code message: drinkovaltine
     """Search for nth letter in nth words"""
-    for i in range(0, len(message_list)):
-        count = 0
-        text = ''
-        for letter in message_list[i]:
-            pass  # STUCK RIGHT HERE
+    results = ""
+    # count every nth word
+    word_list = message_list[count::count+1]
+    print(word_list)
+    # count every nth char
+    for word in word_list:
+        # combine into a string
+        results += word[count]
+    # print string
+    print(results)
 
 
 def main():
     """Decipher coded text file"""
     message = load_text("colchester_message.txt")
+    count = int(input("How many steps to count word/char? "))
     # strip punctuation, whitespace, and newlines from message
     exclude = set(string.punctuation)
     message = ''.join(ch for ch in message if ch not in exclude)  # help from Ashish Cherian on Stack Overflow
-    message = message.replace('\n', '').replace("  ", " ")
-
+    message = message.replace('\n', '').replace("  ", " ")  # yuck; need something better here
     message_list = message.split(" ")
-    print(message_list)
 
-    solve_cipher(message_list)
+    solve_cipher(message_list, count)
 
 
 if __name__ == "__main__":
